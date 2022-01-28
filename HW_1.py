d68efbca4f4face1cbdb85bd93cde5a7dc4b6f84
Lenuara <keypost777@gmail.com>
2022-01-28 17:49:19 +0300

    Add comments in HW_1.py

--- HW_1.py
+++ HW_1.py
@@ -2,20 +2,17 @@
 # HomeWork 1
 
 # Переменные
-my_string = 'Python'
-my_int = 3
-my_float = 3.1415
-my_bytes = bytes(100)
-my_list = [1, 2, 33, 'script', ['a', 'b', 'c'], {"key0": 123, "key1": "program", "key3": ['qw', 'er', 12] }]
-my_tuple = ('q', 'w', 'r', 't', 'y')
-my_set = {'qq', 'ww', '123', 456}
-my_frozen_set = frozenset(my_set)
-my_dict = {'name': 'Vasya', 'age': 25}
-string_1 = 'Quality'
-string_2 = 'assurance'
-union_string = string_1 + ' ' + string_2
+my_string = 'Python'    # 1) Создать переменную типа String
+my_int = 3              # 2) Создать переменную типа Integer
+my_float = 3.1415       # 3) Создать переменную типа Float
+my_bytes = bytes(100)   # 4) Создать переменную типа Bytes
+my_list = [1, 2, 33, 'script', ['a', 'b', 'c'], {"key0": 123, "key1": "program", "key3": ['qw', 'er', 12] }]  # 5) Создать переменную типа List
+my_tuple = ('q', 'w', 'r', 't', 'y')    # 6) Создать переменную типа Tuple
+my_set = {'qq', 'ww', '123', 456}       # 7) Создать переменную типа Set
+my_frozen_set = frozenset(my_set)       # 8) Создать переменную типа Frozen set
+my_dict = {'name': 'Vasya', 'age': 25}  # 9) Создать переменную типа Dict
 
-# Вывод в консоль
+# 10) Вывод в консоль переменных и их типов данных
 print(type(my_string), my_string)
 print(type(my_int), my_int)
 print(type(my_float), my_float)
@@ -25,6 +22,11 @@ print(type(my_tuple), my_tuple)
 print(type(my_set), my_set)
 print(type(my_frozen_set), my_frozen_set)
 print(type(my_dict), my_dict)
-print(union_string)
-print(my_string, my_int)
-print(my_string + ' ' + str(my_int))
+
+string_1 = 'Quality'    # 11) Создать переменную String
+string_2 = 'assurance'  # 11) Создать переменную String
+union_string = string_1 + ' ' + string_2 # 11) Cоздать переменную в которой сканкатенируете эти переменные
+print(union_string)     # 11) Вывести в консоль.
+
+print(my_string, my_int)  # 12) Вывести в одну строку переменные типа String и Integer используя “,” (Запятую)
+print(my_string + ' ' + str(my_int))  # 13) Вывести в одну строку переменные типа String и Integer используя “+” (Плюс)
